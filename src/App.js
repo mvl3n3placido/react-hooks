4e7af2418305576d0443372318e3221b58f28ee5
Mylene Placido <myleneplacido@Mylenes-MacBook-Pro.local>
2019-05-15 13:36:10 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,99 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  // state
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=react');
+  const [loading, setLoading] = useState(false);
+  // fetch news
+  const fecthNews = () => {
+    /* set loading to true */
+    setLoading(true);
+    fetch(url)
+      .then(res => res.json())
+      .then(data => (setNews(data.hits), setLoading(false)))
+      .catch(e => console.log(e))
   }
-}
+   // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    fecthNews();
+  }, [url])
 
-export default App;
+  const handleChange = e => {
+    setSearchQuery(e.target.value)
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
+
+  const showLoading = () => loading ? <h2>Loading...</h2> : '' ;
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      <button>Search</button>
+    </form>
+  );
+
+  const showNews = () => (
+    news.map((n, i) => (
+      <p key={i}>{n.title}</p>))
+  );
+
+  return (
+    <div>
+      <h2>News</h2>
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
+    </div>
+  )
+};
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   const increment = () => {
+//     setCount(count + 1)
+//   }
+//    /*runs everytime the state changes*/
+//   useEffect(()=>{
+//     document.title = `Clicked ${count} times`;
+//   })
+
+//   return ( 
+//     <div>
+//       <h2>counter app</h2>
+//       <button onClick={increment}>Clicked {count} times</button>
+//     </div>
+//   );
+// }
+
+/*without using useState and useEffect hooks */
+// class App extends Component {
+//   state = {
+//     count: 0
+//   };
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+//   componentDidMount() {
+//     document.title = `Clicked ${this.state.count} times`;
+//   }
+//   componentDidUpdate() {
+//     document.title = `Clicked ${this.state.count} times`;
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h2>counter app</h2>
+//         <button onClick={this.increment}>Clicked {this.state.count} times</button>
+//       </div>
+//     );
+//   }
+// }
+
+export default App; 
